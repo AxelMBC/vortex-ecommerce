4b5407e729540b304c8e918d6b9954a76cde76b9
AxelMBC <barrazaaxel2000@gmail.com>
2025-04-07 22:03:25 -0600

    librerias que no se usan han sido comentadas

--- app/carrito/page.tsx
+++ app/carrito/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useSelector } from "react-redux";
-import { RootState } from "@/lib/store";
-import Carrito from "./Carrito/Carrito";
+// import { useSelector } from "react-redux";
+// import { RootState } from "@/lib/store";
+// import Carrito from "./Carrito/Carrito";
 import CarritoDebug from "../components/CarritoDebug";
 
 const CarritoPage = () => {
