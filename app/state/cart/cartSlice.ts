ca22d5c1c37ad270542ca9f3e6ff5de955a9540f
AxelMBC <barrazaaxel2000@gmail.com>
2025-04-28 01:18:03 -0600

    types fix

--- app/state/cart/cartSlice.ts
+++ app/state/cart/cartSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { productoType } from '@/app/types/productoType';
+import { productoType } from '@/types/productoType';
 
 interface CartState {
   items: productoType[];
